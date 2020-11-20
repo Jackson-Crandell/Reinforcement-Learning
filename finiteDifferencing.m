3f9794c824d01ed29d6170df4769e8adc8a88786
Jackson-Crandell <jackson.crandell@mac.com>
2020-11-20 16:05:08 -0500

    Code Changes

--- finiteDifferencing.m
+++ finiteDifferencing.m
@@ -4,30 +4,37 @@ A = [.01];
 B = [.01];
 Q = [.01];
 R = [.01];
-Horizon = 300; %N
-rollouts = 100; %M
+dt = 0.001;
+
 % Optimal Control Gain
 [K_LQR,S,E] = dlqr(A,B,Q,R)
+
+Horizon = 300; %N
+rollouts = 100; %M
 u = zeros(1,Horizon-1);
 x = zeros(1,Horizon);
 r = zeros(1,Horizon);
-J = 0;
+J = zeros(1,Horizon);
 delta_theta = zeros(1,rollouts);
 delta_J = zeros(1,rollouts);
-for j = 1:iter
-    for m = 1:rollouts
-        for k = 1:Horizon
-            delta_theta(:,m) = randn;
-            x(:,k+1) = x(:,k) + A * dt + B * u(:,k) * dt + randn; %Trajectory
-            r(:,k) = x'*Q*x + u'*R*u; %Cost function
-            new_J = r(:,k);
-            delta_J(:,m) = new_J - J;
-            J = new_J;
-            grad_J = (inv(delta_theta' * delta_theta)) * delta_theta' * delta_J;
-            theta = theta + grad_J;
-            u(:,k) = theta * x(:,k); %Add noise? u = Kx
-            expected_reward = 1/iter * sum(r(:,:));
-        end
-    end
+theta = randn; %Randomly initialize theta
+expected_reward = zeros(1,rollouts)
 
+while true % Change to while loop and add stopping condition
+    for m = 1:rollouts
+        delta_theta(:,m) = randn;
+        theta = theta + delta_theta(:,m);  
+        for k = 1:Horizon
+            u(:,k) = u(:,k) * theta;
+            x(:,k+1) = x(:,k) + A * dt + B * u(:,k) * dt; %Trajectory
+            r(:,k) = x(:,k)'*Q*x(:,k) + u(:,k)'*R*u(:,k); %Cost function
+        end
+        new_J = r';
+        delta_J(:,m) = new_J - J;
+        J = new_J;
+        % Add stopping condition
+        expected_reward(:,m) = 1/iter * sum(r);
+    end
+    grad_J = (inv(delta_theta(:,m)' * delta_theta(:,m))) * delta_theta' * delta_J;
+    theta = theta + grad_J
 end
